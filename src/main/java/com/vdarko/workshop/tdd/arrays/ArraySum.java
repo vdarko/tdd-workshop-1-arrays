51b61514a4398ee51288fa7f877c1f7a9caef7da
darkovasilev <darko.vasilev@pannonia-expertise.net>
2020-12-10 22:28:50 +0100

    #1 adding dummy ArraySum and ArraySumTest

---
+++ src/main/java/com/vdarko/workshop/tdd/arrays/ArraySum.java
@@ -0,0 +1,15 @@
+package com.vdarko.workshop.tdd.arrays;
+
+public class ArraySum {
+
+  /**
+   * Calculates sum of array
+   *
+   * @param numbers array of integer numbers
+   *
+   * @return sum of array or <code>null</code> if array is <code>null</code>
+   */
+  public Integer sum(Integer[] numbers) {
+      return null;
+  }
+}
---
+++ src/test/java/com/vdarko/workshop/tdd/arrays/ArraySumTest.java
@@ -0,0 +1,24 @@
+package com.vdarko.workshop.tdd.arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class ArraySumTest {
+
+  /**
+   * <b>GIVEN</b> array of integer values</br>
+   * <b>WHEN</b> sum is calculated</br>
+   * <b>THEN</b> sum value is as expected</br>
+   */
+  @Test
+  public void testSum() {
+
+    // GIVEN
+    //numbers
+
+    // WHEN
+    // sum is calculated
+
+    // THEN
+    // assert sum that it is equal to the expected value
+  }
+}
