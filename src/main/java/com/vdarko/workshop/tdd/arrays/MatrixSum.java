a017c823429608ee4a70e5951e55820d3edf40c8
darkovasilev <darko.vasilev@pannonia-expertise.net>
2020-12-10 22:33:25 +0100

    #2 adding dummy MatrixSum and MatrixSumTest

---
+++ src/main/java/com/vdarko/workshop/tdd/arrays/MatrixSum.java
@@ -0,0 +1,21 @@
+package com.vdarko.workshop.tdd.arrays;
+
+public class MatrixSum {
+
+  private final ArraySum arraySum;
+
+  public MatrixSum() {
+    arraySum = new ArraySum();
+  }
+
+  /**
+   * Calculates sum of matrix (2 dimensional array )
+   *
+   * @param numbers matrix of integer numbers
+   *
+   * @return sum of matrix or <code>null</code> if matrix is <code>null</code>
+   */
+  public Integer sum(Integer[][] numbers) {
+    return null; 
+  }
+}
